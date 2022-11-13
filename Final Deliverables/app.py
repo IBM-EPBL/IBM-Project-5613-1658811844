daa70d746ddc797451fda293f0de1bd03b0dc115
sowmyasrinisan <113987767+sowmyasrinisan@users.noreply.github.com>
2022-11-13 23:53:38 +0530

    Update app.py

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -2,6 +2,6 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def bank():
-    return render_template('ChatBot.html')
+    return render_template('Chatbot.html')
 if __name__ =='__main__':
     app.run(debug = True)
